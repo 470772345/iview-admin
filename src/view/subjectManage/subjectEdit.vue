db38c603fe2c57bdf9c5f60da0b86286d2b65b04
470772345 <470772345@qq.com>
2020-07-15 23:52:21 +0800

    编辑题目

--- src/view/subjectManage/subjectEdit.vue
+++ src/view/subjectManage/subjectEdit.vue
@@ -4,38 +4,32 @@
         <FormItem label="题目内容" prop="name">
             <Input v-model="formValidate.name" type="textarea" :rows="4" placeholder="请输入题目内容"/>
         </FormItem>
+        <FormItem label="题目类型:" prop="name">
+            <Select v-model="optType" style="width:200px">
+               <Option v-for="item in types" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+        </FormItem>
         <FormItem label="题目选项" prop="age">
           <div class="anwser-item" v-for="(item,index) in anwserList" :key="item.id">
             <Input v-model="item.answer" type="textarea" :rows="2"  placeholder="请输入选项内容"/>
             <div class="img-upload">
-            <Upload ref="uploader" name="file" :type="(readonly || !isImage)?'select':type" :action="action" :multiple="multiple" :accept="acceptType" :max-size="maxSize" :show-upload-list="showUploadList" :default-file-list="defaultList" :on-success="handleSuccess" :on-error="handleError" :on-exceeded-size="handleMaxSize" :before-upload="handleBeforeUpload" :on-remove="handleRemove" v-show="!isImage || (isImage && fileList.length<maxLength)" class="posi-rela">
-                <input v-if="isEdit" ref="getImageInput" type="file" @change="inputOnchange" class="hiddenInput" @click="replaceClickEvent" />
-                <div class="upload-block" v-if="!readonly && isImage" :style="{width:iWidth,height:iHeight,lineHeight:iHeight}">
-                    <Icon type="ios-camera" size="20"></Icon>
-                </div>
-                <Button icon="ios-cloud-upload-outline" v-if="!readonly && !isImage">{{btnText}}</Button>
-            </Upload>
+              <Upload ref="uploader" name="file" :type="(readonly || !isImage)?'select':type" :action="action" :multiple="multiple" :accept="acceptType" :max-size="maxSize" :show-upload-list="showUploadList" :default-file-list="defaultList" :on-success="handleSuccess" :on-error="handleError" :on-exceeded-size="handleMaxSize" :before-upload="handleBeforeUpload" :on-remove="handleRemove" v-show="!isImage || (isImage && fileList.length<maxLength)" class="posi-rela">
+                  <input v-if="isEdit" ref="getImageInput" type="file" @change="inputOnchange" class="hiddenInput" @click="replaceClickEvent" />
+                  <div class="upload-block" v-if="!readonly && isImage" :style="{width:iWidth,height:iHeight,lineHeight:iHeight}">
+                      <Icon type="ios-camera" size="20"></Icon>
+                  </div>
+                  <Button icon="ios-cloud-upload-outline" v-if="!readonly && !isImage">{{btnText}}</Button>
+              </Upload>
             </div>
-             <FormItem label="上传图片:" prop="productlogo">
-                    <Upload
-                    action=""
-                    :before-upload="handleUploadicon"
-                    :show-upload-list="false"
-                    :format="['jpg','jpeg','png', 'gif']"
-                    :on-format-error="handleFormatError1">
-                    <img class="iconlabelUrl" :src="formValidate.labelUrl" alt="">
-                    <Input v-model="formValidate.productlogo" disabled style="width: 120px;margin-top:24px" class="left ml5 hidden" />
-                    <!-- <Button type="ghost" icon="ios-cloud-upload-outline">上传文件</Button> -->
-                    </Upload>
-           </FormItem>
+            <div class="set-answer"><Checkbox v-model="item.isAnwser">答案</Checkbox></div>
            <div class="del-btn">
             <Button icon="md-close" type="error" size="small" v-if="index>0" @click="delAnwserItem(index)"></Button>
            </div>
           </div>
           <div class="add-btn"> <Button icon="md-add" type="primary" size="small" @click="addAnwserItem" >添加</Button></div>
         </FormItem>
-         <FormItem label="答案" prop="grade">
-            <Input v-model="formValidate.mail" placeholder="请输入答题"/>
+         <FormItem label="题目解析" prop="grade">
+            <Input v-model="formValidate.mail" type="textarea" :rows="2" placeholder="请输入题目解析"/>
         </FormItem>
         <FormItem>
             <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
@@ -49,14 +43,35 @@ export default {
   name: 'subject-edit', // 题目编辑页
   data () {
     return {
+      optType: '1',
+      single: true,
+      types: [
+        {
+          value: '1',
+          label: '选择题'
+        },
+        {
+          value: '填空题',
+          label: '填空题'
+        },
+        {
+          value: '判断题',
+          label: '判断题'
+        }
+      ],
       anwserList: [
-        { 'index': '0',
+        {
+          'index': '0',
           'question': '',
-          'answer': '111' },
+          'answer': '111',
+          'isAnwser': true
+        },
         {
           'index': '1',
           'question': '',
-          'answer': '222' }
+          'answer': '222',
+          'isAnwser': false
+        }
       ],
       formValidate: {
         name: '',
@@ -70,7 +85,7 @@ export default {
       },
       ruleValidate: {
         name: [
-          { required: true, message: '请输入用户名称', trigger: 'blur' }
+          { required: true, message: '请输入题目内容', trigger: 'blur' }
         ],
         mobile: [
           { required: true, message: '请输入手机号码', trigger: 'blur' }
@@ -131,6 +146,10 @@ export default {
       this.anwserList.splice(index, 1)
     },
     addAnwserItem () {
+      if (this.anwserList.length >= 8) {
+        this.$Message.warning('最多输入8个题目选项~')
+        return
+      }
       let obj = {
         question: '',
         answer: '111'
@@ -160,6 +179,10 @@ export default {
    align-items: center;
    margin-top: 10px;
    display: flex;
+   .set-answer{
+     font-size: 14px;
+     margin-left: 10px;
+   }
    .img-upload{
      margin-left: 10px;
    }
